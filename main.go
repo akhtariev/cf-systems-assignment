eab8de5b1dccce3bef621971f1eb6d0eb1335762
Roman Akhtariev <ackhtariev@gmail.com>
2020-10-10 17:30:56 -0700

    Set up get request

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"net"
+	"fmt"
+	"os"
+	"io/ioutil"
+)
+
+func checkError(err error) {
+    if err != nil {
+        fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+        os.Exit(1)
+    }
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	 // init
+	url := "cloudflare-workers.akhtariev.workers.dev:80"
+
+	//  tcpAddr, err := net.ResolveTCPAddr("tcp4", url)
+	//  checkError(err)
+	 conn, err := net.Dial("tcp", url)
+	 checkError(err)
+  
+	 // send message
+	 _, err = conn.Write([]byte("GET / HTTP/1.1\r\n\r\n"))
+	  checkError(err)
+  
+	 // receive message
+	 result, err := ioutil.ReadAll(conn)
+	 checkError(err)
+	 fmt.Println(string(result))
+     os.Exit(0)
 }
